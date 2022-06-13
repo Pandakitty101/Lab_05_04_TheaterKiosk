e3fdbbd5b600e14ccca9ef812576adf7e80e5686
Pandakitty101 <63027190+Pandakitty101@users.noreply.github.com>
2022-06-12 20:37:26 -0400

    Add files via upload

---
+++ Lab_05_04_TheaterKiosk/src/TheaterKiosk.java
@@ -0,0 +1,12 @@
+public class TheaterKiosk {
+    public static void main(String[] args)
+    {
+        int age = 5;
+        final int THEATER_AGE = 21;
+
+        if (age >= THEATER_AGE)
+        {
+            System.out.println("You get a wristband");
+        }
+    }
+}
